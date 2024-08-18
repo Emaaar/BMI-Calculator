22ac38e85b6321bada1ec0ebdc1b550490effccc
Mary Mhar Perez <marymharperez@gmail.com>
2024-08-18 22:44:14 +0800

    first commit

---
+++ App11/App11/Page1.xaml.cs
@@ -0,0 +1,12 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App11
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
